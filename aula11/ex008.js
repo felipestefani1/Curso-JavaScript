5b5b4eea8bfc3a2aa65a16f701e1bb1f323f7d78
felipestefani1 <141695217+felipestefani1@users.noreply.github.com>
2023-10-02 20:53:53 -0300

    js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+if(vel > 60){
+    console.log (`você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
