7b55d258644dba944d135f0b3b1c87380aa7e041
felipestefani1 <141695217+felipestefani1@users.noreply.github.com>
2023-10-03 21:37:47 -0300

    js

---
+++ aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 20
+if (idade < 16) {
+    console.log('menor de idade')}
+else if (idade < 18 || idade > 65){
+        console.log('voto opcional')
+} else {
+    console.log('voto obrigatório')+565+6
+}
+
+
